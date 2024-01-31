6bda8a6181344587261a8f1be31fac0a72dfdf30
moipav <moipav@ya.ru>
2024-01-31 13:52:09 +0800

    обновил версию easyAdmin, изначалььно встала 3 и не хотела дружить с symfony 6 долго провозился

---
+++ src/Controller/Admin/TariffCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Tariff;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class TariffCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Tariff::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
